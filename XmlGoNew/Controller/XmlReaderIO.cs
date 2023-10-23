f279d3a045a04a37b3b2d704cd4b4e01fbc9457d
Rafael dos Santos Fracasso <rafael.fracasso@icloud.com>
2023-10-23 16:54:20 -0300

    Adicionar arquivos de projeto.

---
+++ XmlGoNew/Controller/XmlReaderIO.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using XmlGoNew.Model;
+using System.Linq;
+using System.Xml.Serialization;
+using System.Xml;
+
+namespace XmlGoNew.Controller
+{
+    internal class XmlReaderIO
+    {
+        public static List<Xml> getXmlData(string xmlPath)
+        {
+
+                    string xml = System.IO.File.ReadAllText(xmlPath);
+                    byte[] byteArray = Encoding.UTF8.GetBytes(xml);
+
+                    MemoryStream ms = new MemoryStream(byteArray);
+                    XmlReader xr = XmlReader.Create(ms);
+
+                    Xml model = new Xml();
+                    List<Xml> listReader = new List<Xml>();
+
+                    while (xr.Read())
+                    {
+                        if(xr.NodeType == XmlNodeType.Element && xr.Name == "Element" && xr.HasAttributes == true)
+                        {
+                            string id = xr.GetAttribute(0);
+                            model.id = id;
+                        }
+                        if(xr.NodeType == XmlNodeType.Element && xr.Name == "title")
+                        {
+                            model.title = xr.ReadElementContentAsString();
+                        }
+                        if(xr.NodeType == XmlNodeType.Element && xr.Name == "module")
+                        {
+                            model.module = xr.ReadElementContentAsString();
+                        }
+                        if (xr.NodeType == XmlNodeType.Element && xr.Name == "value")
+                        {
+                            model.value = xr.ReadElementContentAsString();
+                        }
+                        if(xr.NodeType == XmlNodeType.Element && xr.Name == "info")
+                        {
+                            model.info = xr.ReadElementContentAsString();
+                            listReader.Add(model);
+                            model = new Xml();
+                        }
+                    }
+                    return listReader;
+        }
+
+      
+
+    }
+
+}
---
+++ XmlGoNew/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Xml.Linq;
+using XmlGoNew.Controller;
+using XmlGoNew.Model;
+
+foreach (Xml element in XmlReaderIO.getXmlData(@"C:\Users\fraca\source\repos\XmlGoNew\XmlGoNew\File\stuffs.xml"))
+{
+    Console.WriteLine(element.id);
+    Console.WriteLine(element.title);
+    Console.WriteLine(element.module);
+    Console.WriteLine(element.value);
+    Console.WriteLine(element.info);
+
+    Console.WriteLine("******************************************************");
+
+}
